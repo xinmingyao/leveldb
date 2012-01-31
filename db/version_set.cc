14478f170bbe3d13bc0119d41b70e112b3925453
Dave Smith <dizzyd@dizzyd.com>
2012-01-31 07:07:26 -0700

    Rework compaction scoring to ensure L1 doesn't exceed the max bytes on that level
    With this change, we are now special casing both L1 and L0 in hopes of
    keeping the total compaction size of either level down to something
    managable.

--- db/version_set.cc
+++ db/version_set.cc
@@ -932,40 +932,41 @@ void VersionSet::Finalize(Version* v) {
   int best_level = -1;
   double best_score = -1;
 
-  for (int level = 0; level < config::kNumLevels-1; level++) {
-    double score;
-    if (level == 0) {
-      // We treat level-0 specially by bounding the number of files
-      // instead of number of bytes for two reasons:
-      //
-      // (1) With larger write-buffer sizes, it is nice not to do too
-      // many level-0 compactions.
-      //
-      // (2) The files in level-0 are merged on every read and
-      // therefore we wish to avoid too many files when the individual
-      // file size is small (perhaps because of a small write-buffer
-      // setting, or very high compression ratios, or lots of
-      // overwrites/deletions).
-      if (v->files_[level].size() > config::kL0_CompactionTrigger) {
-          best_level = 0;
-          best_score = 100000;
-          break;
-      } else {
-          score = v->files_[level].size() /
-              static_cast<double>(config::kL0_CompactionTrigger);
+  if (TotalFileSize(v->files_[1]) > MaxBytesForLevel(1)) {
+      best_level = 1;
+      best_score = 1000000;
+  } else if (v->files_[0].size() > config::kL0_CompactionTrigger) {
+      best_level = 0;
+      best_score = 1000000;
+  } else {
+      for (int level = 0; level < config::kNumLevels-1; level++) {
+          double score;
+          if (level == 0) {
+              // We treat level-0 specially by bounding the number of files
+              // instead of number of bytes for two reasons:
+              //
+              // (1) With larger write-buffer sizes, it is nice not to do too
+              // many level-0 compactions.
+              //
+              // (2) The files in level-0 are merged on every read and
+              // therefore we wish to avoid too many files when the individual
+              // file size is small (perhaps because of a small write-buffer
+              // setting, or very high compression ratios, or lots of
+              // overwrites/deletions).
+              score = v->files_[level].size() /
+                  static_cast<double>(config::kL0_CompactionTrigger);
+          } else {
+              // Compute the ratio of current size to size limit.
+              const uint64_t level_bytes = TotalFileSize(v->files_[level]);
+              score = static_cast<double>(level_bytes) / MaxBytesForLevel(level);
+          }
+
+          if (score > best_score) {
+              best_level = level;
+              best_score = score;
+          }
       }
-    } else {
-      // Compute the ratio of current size to size limit.
-      const uint64_t level_bytes = TotalFileSize(v->files_[level]);
-      score = static_cast<double>(level_bytes) / MaxBytesForLevel(level);
-    }
-
-    if (score > best_score) {
-      best_level = level;
-      best_score = score;
-    }
   }
-
   v->compaction_level_ = best_level;
   v->compaction_score_ = best_score;
 }
